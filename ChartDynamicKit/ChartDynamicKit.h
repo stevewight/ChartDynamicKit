9d43057090fc6140f5a763cb030d970e490a7939
Steve Wight <stevendouglaswight@gmail.com>
2018-06-06 23:58:21 +0200

    add project

---
+++ ChartDynamicKit/ChartDynamicKit.h
@@ -0,0 +1,19 @@
+//
+//  ChartDynamicKit.h
+//  ChartDynamicKit
+//
+//  Created by Steve on 6/6/18.
+//  Copyright © 2018 Steve Wight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChartDynamicKit.
+FOUNDATION_EXPORT double ChartDynamicKitVersionNumber;
+
+//! Project version string for ChartDynamicKit.
+FOUNDATION_EXPORT const unsigned char ChartDynamicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChartDynamicKit/PublicHeader.h>
+
+
